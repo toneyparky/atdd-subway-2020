5f11a84e454d6d44a0e15e0f2f40b8f3af257c07
toneyparky <qkrtmddhks95@gmail.com>
2020-09-06 17:17:29 +0900

    refactor: 경로 조회 문서화를 수정한다.
    - PathDocumentation이 MapControllerTest를 이용하도록 수정

--- src/test/java/wooteco/subway/maps/map/documentation/PathDocumentation.java
+++ src/test/java/wooteco/subway/maps/map/documentation/PathDocumentation.java
@@ -1,74 +1,42 @@
 package wooteco.subway.maps.map.documentation;
 
-import static io.restassured.module.mockmvc.RestAssuredMockMvc.*;
 import static org.springframework.restdocs.headers.HeaderDocumentation.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 import org.springframework.restdocs.payload.JsonFieldType;
-import org.springframework.web.context.WebApplicationContext;
 
-import wooteco.security.core.TokenResponse;
 import wooteco.subway.common.documentation.Documentation;
-import wooteco.subway.maps.map.application.MapService;
-import wooteco.subway.maps.map.application.PathService;
 import wooteco.subway.maps.map.ui.MapController;
 
 @WebMvcTest(controllers = {MapController.class})
 public class PathDocumentation extends Documentation {
-	@Autowired
-	private MapController mapController;
-
-	@MockBean
-	private MapService mapService;
-	@MockBean
-	private PathService pathService;
-
-	protected TokenResponse tokenResponse;
-
-	@BeforeEach
-	public void setUp(WebApplicationContext context, RestDocumentationContextProvider restDocumentation) {
-		super.setUp(context, restDocumentation);
-		tokenResponse = new TokenResponse("token");
-	}
 
 	@DisplayName("경로를 찾는다.")
 	@Test
-	void findPath() {
-
-		Map<String, Object> params = new HashMap<>();
-		params.put("source", 1L);
-		params.put("target", 2L);
-		given().log().all().
-			header("Authorization", "Bearer " + tokenResponse.getAccessToken()).
-			contentType(MediaType.APPLICATION_JSON_VALUE).
-			body(params).
-			param("source", "1").
-			param("target", "3").
-			param("type", "DISTANCE").
-			when().
-			get("/paths").
-			then().
-			log().all().
-			apply(document("paths/find",
-				getDocumentRequest(),
-				getDocumentResponse(),
-				requestHeaders(
-					headerWithName("Authorization").description("Bearer auth credentials")),
-				requestFields(
-					fieldWithPath("source").type(JsonFieldType.NUMBER).description("출발역 아이디"),
-					fieldWithPath("target").type(JsonFieldType.NUMBER).description("도착역 아이디")))).
-			extract();
+	public static RestDocumentationResultHandler findPath() {
+		return document("paths/find",
+			getDocumentRequest(),
+			getDocumentResponse(),
+			requestHeaders(
+				headerWithName("Authorization").description("사용자 토큰 정보")),
+			requestParameters(
+				parameterWithName("source").description("출발역 아이디"),
+				parameterWithName("target").description("도착역 아이디"),
+				parameterWithName("type").description("경로 검색 기준")),
+			responseFields(
+				fieldWithPath("stations.[]").type(JsonFieldType.ARRAY).description("경로에 해당 되는 역 목록"),
+				fieldWithPath("stations.[].id").type(JsonFieldType.NUMBER).description("경로에 해당 되는 역 ID"),
+				fieldWithPath("stations.[].name").type(JsonFieldType.STRING).description("경로에 해당 되는 역 이름"),
+				fieldWithPath("duration").type(JsonFieldType.NUMBER).description("경로 예상 시간(분)"),
+				fieldWithPath("distance").type(JsonFieldType.NUMBER).description("경로 예상 거리(KM)"),
+				fieldWithPath("fare").type(JsonFieldType.NUMBER).description("요금")
+			)
+		);
 	}
 }
--- src/test/java/wooteco/subway/maps/map/ui/MapControllerTest.java
+++ src/test/java/wooteco/subway/maps/map/ui/MapControllerTest.java
@@ -1,39 +1,98 @@
 package wooteco.subway.maps.map.ui;
 
-import wooteco.subway.maps.map.application.MapService;
-import wooteco.subway.maps.map.domain.PathType;
-import wooteco.subway.maps.map.dto.PathResponse;
-import wooteco.subway.members.member.domain.LoginMember;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.*;
+import wooteco.security.core.TokenResponse;
+import wooteco.subway.maps.map.application.MapService;
+import wooteco.subway.maps.map.documentation.PathDocumentation;
+import wooteco.subway.maps.map.dto.PathResponse;
+import wooteco.subway.maps.station.dto.StationResponse;
 
-public class MapControllerTest {
-    @Test
-    void findPath() {
-        MapService mapService = mock(MapService.class);
-        MapController controller = new MapController(mapService);
-        when(mapService.findPath(anyLong(), anyLong(), any(), any())).thenReturn(new PathResponse());
+@WebMvcTest(controllers = MapController.class)
+@ExtendWith(RestDocumentationExtension.class)
+public class MapControllerTest extends PathDocumentation {
 
-        ResponseEntity<PathResponse> entity = controller.findPath(new LoginMember(1L, "a@a", "qw", 20),1L, 2L, PathType.DISTANCE);
+	@MockBean
+	private MapService mapService;
 
-        assertThat(entity.getBody()).isNotNull();
-    }
+	private MockMvc mockMvc;
 
-    @Test
-    void findMap() {
-        MapService mapService = mock(MapService.class);
-        MapController controller = new MapController(mapService);
+	private List<StationResponse> stations;
+	private int duration;
+	private int distance;
+	private int fare;
 
-        ResponseEntity entity = controller.findMap();
+	protected TokenResponse tokenResponse;
 
-        assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        verify(mapService).findMap();
-    }
+	@BeforeEach
+	public void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider contextProvider) {
+		super.setUp(webApplicationContext, contextProvider);
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+			.apply(documentationConfiguration(contextProvider))
+			.addFilters(new CharacterEncodingFilter("UTF-8", true))
+			.alwaysDo(print())
+			.build();
+
+		stations = Arrays.asList(
+			new StationResponse(1L, "강남역", LocalDateTime.now(), LocalDateTime.now()),
+			new StationResponse(2L, "교대역", LocalDateTime.now(), LocalDateTime.now())
+		);
+
+		duration = 1;
+		distance = 3;
+		fare = 1250;
+
+		tokenResponse = new TokenResponse("token");
+	}
+
+	@DisplayName("경로를 조회한다.")
+	@Test
+	void findPaths() throws Exception {
+		when(mapService.findPath(anyLong(), anyLong(), any(), any())).thenReturn(
+			new PathResponse(stations, duration, distance, fare));
+
+		mockMvc.perform(get("/paths")
+			.header("Authorization", "Bearer" + tokenResponse.getAccessToken())
+			.queryParam("source", "1")
+			.queryParam("target", "2")
+			.queryParam("type", "DISTANCE")
+		).andExpect(status().isOk())
+			.andDo(PathDocumentation.findPath());
+	}
+
+	@Test
+	void findMap() {
+		MapService mapService = mock(MapService.class);
+		MapController controller = new MapController(mapService);
+
+		ResponseEntity entity = controller.findMap();
+
+		assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+		verify(mapService).findMap();
+	}
 }
