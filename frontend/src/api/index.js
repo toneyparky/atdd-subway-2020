b72d58ed07e6d3d33a6eede4ac3641dca558e463
toneyparky <qkrtmddhks95@gmail.com>
2020-09-06 20:10:24 +0900

    refactor: api에 들어가는 Authorization 헤더를 상수화한다.

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -2,40 +2,30 @@ import Vue from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
+const AUTH_CONFIG = {
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem('token')}` || ''
+  }
+};
+
 const ApiService = {
   init() {
     Vue.use(VueAxios, axios)
   },
   get(uri) {
-    return Vue.axios.get(`${uri}`, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}` || ''
-      }
-    })
+    return Vue.axios.get(`${uri}`, AUTH_CONFIG)
   },
   login(uri, config) {
     return Vue.axios.post(`${uri}`, {}, config)
   },
   post(uri, params) {
-    return Vue.axios.post(`${uri}`, params, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}` || ''
-      }
-    })
+    return Vue.axios.post(`${uri}`, params, AUTH_CONFIG)
   },
   update(uri, params) {
-    return Vue.axios.put(uri, params, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}` || ''
-      }
-    })
+    return Vue.axios.put(uri, params, AUTH_CONFIG)
   },
   delete(uri) {
-    return Vue.axios.delete(uri, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}` || ''
-      }
-    })
+    return Vue.axios.delete(uri, AUTH_CONFIG)
   }
 }
 
